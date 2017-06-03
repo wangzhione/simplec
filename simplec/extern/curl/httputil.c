f9ba4a1b343aae1aecd8faa0d39fde3d82e31d6f
wangzhi <wangzhione@163.com>
2017-06-03 17:02:08 +0800

    提交http结构模块

--- simplec/extern/curl/httputil.c
+++ simplec/extern/curl/httputil.c
@@ -1,2 +1,49 @@
-#include <httputil.h>
+﻿#include <httputil.h>
+
+//
+// http_start - http 库启动
+// return	: void
+//
+void 
+http_start(void) {
+
+}
+
+//
+// http_get - http get 请求 or https get请求 
+// url		: 请求的url
+// str		: 返回最终申请的结果, NULL表示只请求不接收数据
+// return	: true表示请求成功
+//
+bool 
+http_get(const char * url, tstr_t str) {
+
+	return true;
+}
+
+bool 
+http_sget(const char * url, tstr_t str) {
+
+	return true;
+}
+
+//
+// http_post - http post 请求 or https post 请求
+// url		: 请求的路径
+// params	: 参数集 key=value&.... 串
+// str		: 返回结果, 需要自己释放, NULL表示只请求不接受数据返回
+// return	: false表示请求失败, 会打印错误信息
+//
+bool 
+http_post(const char * url, const char * params, tstr_t str) {
+	
+	return true;
+}
+
+bool 
+http_spost(const char * url, const char * params, tstr_t str) {
+
+
+	return true;
+}
 
--- simplec/extern/curl/include/httputil.h
+++ simplec/extern/curl/include/httputil.h
@@ -1,6 +1,33 @@
-#ifndef _H_SIMPLEC_HTTPUTIL
+﻿#ifndef _H_SIMPLEC_HTTPUTIL
 #define _H_SIMPLEC_HTTPUTIL
 
+#include <tstr.h>
+#include <md5util.h>
 #include <curl/curl.h>
+
+//
+// http_start - http 库启动
+// return	: void
+//
+extern void http_start(void);
+
+//
+// http_get - http get 请求 or https get请求 
+// url		: 请求的url
+// str		: 返回最终申请的结果, NULL表示只请求不接收数据
+// return	: true表示请求成功
+//
+extern bool http_get(const char * url, tstr_t str);
+extern bool http_sget(const char * url, tstr_t str);
+
+//
+// http_post - http post 请求 or https post 请求
+// url		: 请求的路径
+// params	: 参数集 key=value&.... 串
+// str		: 返回结果, 需要自己释放, NULL表示只请求不接受数据返回
+// return	: false表示请求失败, 会打印错误信息
+//
+extern bool http_post(const char * url, const char * params, tstr_t str);
+extern bool http_spost(const char * url, const char * params, tstr_t str);
 
 #endif // !_H_SIMPLEC_HTTPUTIL
